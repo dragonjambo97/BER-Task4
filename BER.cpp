2f8477597ac2d25e46fe5b13abe0e60ce0faf1cc
dragonjambo97 <jankiewicz.dawid@gmail.com>
2022-06-25 15:53:58 +0200

    Create BER.cpp

---
+++ BER.cpp
@@ -0,0 +1,47 @@
+#include <tuple>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <bitset>
+using namespace std;
+
+tuple<int, int, float, long> makeComparison(fstream &file_A, fstream &file_B){
+
+    int roznica = 0, porownanie = 0;
+    long sizeInBytes = 0L;
+    float ber = 0.;
+    char a, b;
+    string bitSetA, bitSetB;
+
+    createLog("Rozpoczêto analizê.");
+    auto start = chrono::high_resolution_clock::now();
+
+    while(!file_A.eof()){
+
+        a = file_A.get();
+        b = file_B.get();
+        sizeInBytes++;
+
+        if(a != b){
+            bitSetA = bitset<8>(a).to_string();
+            bitSetB = bitset<8>(b).to_string();
+
+            for(int i = 7; i >= 0; i--){
+                if(bitSetA[i] != bitSetB[i]) diff++;
+                porownanie++;
+            }
+        }
+    }
+
+    sizeInBytes -= 1;
+    ber = float(roznica) / (sizeInBytes * 8.) * 100.;
+
+    auto stop = chrono::high_resolution_clock::now();
+    createLog("Zakoñczono.");
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+
+    tuple<int, int, float, long> results = make_tuple(porownanie, roznica, ber, duration.count());
+    return results;
+}
+
